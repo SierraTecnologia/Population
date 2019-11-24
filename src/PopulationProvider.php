53d3152c35b59467a1c15d430c9991119dd47d82
Ricardo Rebello Sierra <contato@ricardosierra.com.br>
2019-11-24 00:02:36 -0300

    Provider

---
+++ src/PopulationProvider.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Population;
+
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\ServiceProvider;
+
+class PopulationProvider extends ServiceProvider
+{
+    public static $providers = [
+        // \Population\Providers\PopulationEventServiceProvider::class,
+        // \Population\Providers\PopulationServiceProvider::class,
+        // \Population\Providers\PopulationRouteProvider::class,
+
+        \Audit\AuditProvider::class,
+        \Casa\CasaProvider::class,
+    ];
+
+    /**
+     * Alias the services in the boot.
+     */
+    public function boot()
+    {
+        // $this->publishes([
+        //     __DIR__.'/Publishes/resources/tools' => base_path('resources/tools'),
+        //     __DIR__.'/Publishes/app/Services' => app_path('Services'),
+        //     __DIR__.'/Publishes/public/js' => base_path('public/js'),
+        //     __DIR__.'/Publishes/public/css' => base_path('public/css'),
+        //     __DIR__.'/Publishes/public/img' => base_path('public/img'),
+        //     __DIR__.'/Publishes/config' => base_path('config'),
+        //     __DIR__.'/Publishes/routes' => base_path('routes'),
+        //     __DIR__.'/Publishes/app/Controllers' => app_path('Http/Controllers/Population'),
+        // ]);
+
+        // $this->publishes([
+        //     __DIR__.'../resources/views' => base_path('resources/views/vendor/Population'),
+        // ], 'SierraTecnologia Population');
+    }
+
+    /**
+     * Register the services.
+     */
+    public function register()
+    {
+        $this->setProviders();
+
+        // // View namespace
+        // $this->loadViewsFrom(__DIR__.'/Views', 'Population');
+
+        // if (is_dir(base_path('resources/Population'))) {
+        //     $this->app->view->addNamespace('Population-frontend', base_path('resources/Population'));
+        // } else {
+        //     $this->app->view->addNamespace('Population-frontend', __DIR__.'/Publishes/resources/Population');
+        // }
+
+        $this->loadMigrationsFrom(__DIR__.'/Migrations');
+
+        // // Configs
+        // $this->app->config->set('Population.modules.Population', include(__DIR__.'/config.php'));
+
+        /*
+        |--------------------------------------------------------------------------
+        | Register the Commands
+        |--------------------------------------------------------------------------
+        */
+
+        $this->commands([]);
+    }
+
+    protected function setProviders()
+    {
+        collection(self::$providers)->map(function ($provider) {
+            $this->app->register($provider);
+        })
+
+    }
+
+}

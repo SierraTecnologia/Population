54c98adce6d9caefce39c093cbed1b6fa810a53f
Ricardo Rebello Sierra <contato@ricardosierra.com.br>
2019-11-26 23:57:28 -0300

    FIx bug

--- src/PopulationProvider.php
+++ src/PopulationProvider.php
@@ -5,6 +5,7 @@ namespace Population;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Collection;
 
 class PopulationProvider extends ServiceProvider
 {
@@ -68,12 +69,11 @@ class PopulationProvider extends ServiceProvider
         $this->commands([]);
     }
 
-    protected function setProviders()
+    private function setProviders()
     {
-        collection(self::$providers)->map(function ($provider) {
+        (new Collection(self::$providers))->map(function ($provider) {
             $this->app->register($provider);
-        })
-
+        });
     }
 
 }

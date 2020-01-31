16d92914d3479380df8ed814447f9e808301a77b
Ricardo R Sierra <ricardo@sierratecnologia.com.br>
2020-01-31 05:03:53 -0300

    Merge Conflito

--- src/Traits/HasPhoto.php
+++ src/Traits/HasPhoto.php
@@ -40,21 +40,11 @@ trait HasPhoto
 
     public function getProfileUrl($altura = false, $largura = false)
     {
-<<<<<<< HEAD
-        $media = $this->getMedia('avatars')->first();
-
-        if (!$media) {
-            return ''; // @todo Botar imagem Padrao
-        }
-
-        return $media->getUrl('thumb');
-=======
         if ($avatar = $this->getMedia('avatars')->first()) {
             return $avatar->getUrl('thumb');
         }
         // @todo diferenciar homem de mulher
         return 'https://connectbuncombe.org/wp-content/uploads/2019/05/woman.png';
->>>>>>> 0730dde03f67e821e3b58dd1f2dac02090e56ebe
     }
 
 }
